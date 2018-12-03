4f1c01934ca230a5a8653b8f0fc113661e4a29c8
kojix2 <2xijok@gmail.com>
2018-12-03 21:28:08 +0900

    add new file laplacian

---
+++ laplacian.rb
@@ -0,0 +1,22 @@
+A = (1..-1).freeze
+B = (0..-2).freeze
+T = true
+
+def laplacian(uv)
+  l_uv = SFloat.zeros 256, 256
+  l_uv[A, T]._ + uv[B, T]
+  l_uv[T, A]._ + uv[T, B]
+
+  l_uv[0, T]._ + uv[-1, T]
+  l_uv[T, 0]._ + uv[T, -1]
+
+  l_uv[B, T]._ + uv[A, T]
+  l_uv[T, B]._ + uv[T, A]
+
+  l_uv[-1, T]._ + uv[0, T]
+  l_uv[T, -1]._ + uv[T, 0]
+
+  l_uv._ - (uv * 4)
+  l_uv._ / (Dx * Dx)
+  l_uv
+end
