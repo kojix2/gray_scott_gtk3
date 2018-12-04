46a9e75e1d8486d52e864231f795690a70087259
kojix2 <2xijok@gmail.com>
2018-12-04 15:41:13 +0900

    module

--- lib/gray_scott_gtk3.rb
+++ lib/gray_scott_gtk3.rb
@@ -1,4 +1,10 @@
+require "numo/narray"
+require "gtk3"
+
 require "gray_scott_gtk3/version"
+require "gray_scott/hoge"
+require "gray_scott/laplacian"
+require "gray_scott/gray_scott"
 
 module GrayScottGtk3
   class Error < StandardError; end
---
+++ lib/gray_scott_gtk3/gray_scott.rb
@@ -0,0 +1,120 @@
+module GrayScottGtk3
+
+  # 短縮
+  class Numo::SFloat
+    alias _ inplace
+  end
+  SFloat = Numo::SFloat
+  UInt8  = Numo::UInt8
+
+  @height = 256
+  @width = 256
+
+  require './hoge'
+
+  # Timeout用秒数 MSEC 設定
+  if ARGV[0]
+    msec = ARGV[0].to_i
+  else
+    puts 'please specify command-line argument. For example `ruby gray_scott.rb 40`'
+    exit
+  end
+  MSEC = msec
+
+  def show_about
+    a = Gtk::AboutDialog.new
+    a.program_name = 'Gray-Scott'
+    a.logo = GdkPixbuf::Pixbuf.new(file: File.join(__dir__, 'screenshot/about_icon.png'))
+    a.authors = ['kojix2']
+    a.run
+    a.destroy
+  end
+
+  def on_f_changed(f)
+    @f = f.value
+  end
+
+  def on_k_changed(k)
+    @k = k.value
+  end
+
+  def display
+    @gimage.pixbuf = to_pixbuf(@show_u ? @u : @v)
+  end
+
+  def display_legend
+    legend = (SFloat.new(1,512).seq * SFloat.ones(24,1)) / 512
+    data = colorize(legend, @color).to_string
+    pixbuf = GdkPixbuf::Pixbuf.new data: data, width: 512, height: 24
+    @legend_image.pixbuf = pixbuf
+  end
+
+  def on_execute_toggled(widget)
+    @doing_now = widget.active?
+    if @doing_now
+      GLib::Timeout.add MSEC do
+        update_u_v
+        display
+        @doing_now
+      end
+    end
+  end
+
+  def on_save_clicked
+    dialog = Gtk::FileChooserDialog.new(title: 'PNG画像を保存',
+                                        action: :save,
+                                        buttons: [%i[save accept], %i[cancel cancel]])
+    dialog.do_overwrite_confirmation = true
+    if dialog.run == :accept
+      filename = dialog.filename
+      @gimage.pixbuf.save(filename, :png)
+    end
+    dialog.destroy
+  end
+
+  def main_quit
+    Gtk.main_quit
+  end
+
+  def on_new_clicked
+    @u.fill 1.0
+    @v.fill 0.0
+    if @show_u && !@doing_now
+      dialog = Gtk::MessageDialog.new(message: 'display V density',
+                                      type: :info,
+                                      tutton_type: :close)
+      dialog.run
+      @uv_combobox.active = 1 # v
+      dialog.destroy
+    end
+    display_legend
+    display
+  end
+
+  def on_uv_combobox_changed(w)
+    @show_u = w.active_text == 'u'
+    display unless @doing_now
+  end
+
+  def on_motion(_widget, e)
+    x = e.x * @width / 512
+    y = e.y * @height / 512
+    r = @pen_radius.value
+    if x > r && y > r && x < (@height - 1 - r) && y < (@width - 1 - r)
+      @v[(y - r)..(y + r), (x - r)..(x + r)] = @pen_density.value
+    end
+    display unless @doing_now
+  end
+
+  builder = Gtk::Builder.new
+  builder.add_from_file 'gray_scott.glade'
+
+  %w[win gimage legend_image uv_combobox pen_density pen_radius].each do |s|
+    instance_variable_set('@' + s.to_s, builder.get_object(s))
+  end
+  builder.connect_signals { |handler| method(handler) }
+
+  @win.show_all
+  Gtk.main
+
+end
