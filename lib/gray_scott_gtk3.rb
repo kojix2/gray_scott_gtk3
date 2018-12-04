96dc599f67df97f34c42b43df9eaaf821ecfd28b
kojix2 <2xijok@gmail.com>
2018-12-04 14:55:54 +0900

    add new files for gem

---
+++ lib/gray_scott_gtk3.rb
@@ -0,0 +1,6 @@
+require "gray_scott_gtk3/version"
+
+module GrayScottGtk3
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gray_scott_gtk3/version.rb
@@ -0,0 +1,3 @@
+module GrayScottGtk3
+  VERSION = "0.1.0"
+end
