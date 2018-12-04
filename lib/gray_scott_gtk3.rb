74eb45de58184e16d7353e0323bf60be7f8c287e
kojix2 <2xijok@gmail.com>
2018-12-04 22:59:44 +0900

    shorthand

--- lib/gray_scott_gtk3.rb
+++ lib/gray_scott_gtk3.rb
@@ -1,8 +1,17 @@
 require 'numo/narray'
 require 'gtk3'
 
+module ShortNumo
+    N = Numo
+    class Numo::SFloat
+      alias _ inplace
+    end
+    SFloat = Numo::SFloat
+    UInt8  = Numo::UInt8
+end
+
+require 'gray_scott_gtk3/model'
 require 'gray_scott_gtk3/gray_scott'
-require 'gray_scott_gtk3/hoge'
 require 'gray_scott_gtk3/laplacian'
 require 'gray_scott_gtk3/version'
 
