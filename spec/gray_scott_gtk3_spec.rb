2d49dad1e2cfb2291e99d57f38be2deb7cfa7349
kojix2 <2xijok@gmail.com>
2018-12-11 17:26:49 +0900

    start using rspec

---
+++ spec/gray_scott_gtk3_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe GrayScottGtk3 do
+  it "has a version number" do
+    expect(GrayScottGtk3::VERSION).not_to be nil
+  end
+end
---
+++ spec/model_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+RSpec.describe GrayScottGtk3::Model do
+  before do
+    @model = GrayScottGtk3::Model.new
+  end
+
+  it "has feed rate" do
+    expect(@model.f).to eq 0.04
+  end
+
+  it "has kill rate" do
+    expect(@model.k).to eq 0.06
+  end
+
+  it "has u" do
+    expect(@model.u).to be_an_instance_of Numo::SFloat
+  end
+
+  it "has v" do
+    expect(@model.v).to be_an_instance_of Numo::SFloat
+  end
+
+  it "respond to clear" do
+    expect(@model).to respond_to :clear
+  end
+
+  it "respond to update" do
+    expect(@model).to respond_to :update
+  end
+end
